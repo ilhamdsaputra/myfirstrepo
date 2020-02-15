014129f9880c21052883c1ad2e25fe746e00699a
ilhamdsaputra <ilhamds121@gmail.com>
2020-02-16 06:32:21 +0700

    Initial Commit. Added coba.java file contains only ‘Hello World

---
+++ coba.java
@@ -0,0 +1,14 @@
+/**
+ * coba
+ */
+public class coba {
+
+    public static void main(String[] args) {
+        System.out.println("Baru mencoba git");
+
+        int a = 2;
+        float b = 3;
+
+        System.out.println("Hasilnya adalah :"+ a+b);
+    }
+}
